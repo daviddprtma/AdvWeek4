0abc678098dfff680569e73ea5532f8148ecd48e
daviddprtma <davidchristianpratama@gmail.com>
2022-03-10 10:39:13 +0700

    Update GSON & Volley Week 5

--- app/src/main/java/id/davidpratama/advweek4/model/Model.kt
+++ app/src/main/java/id/davidpratama/advweek4/model/Model.kt
@@ -1,9 +1,14 @@
 package id.davidpratama.advweek4.model
 
+import com.google.gson.annotations.SerializedName
+
 data class Student(
     val id: String?,
+    @SerializedName("student_name")
     val name: String?,
+    @SerializedName("birth_of_date")
     val bod: String?,
     val phone: String?,
+    @SerializedName("phone_url")
     val photoUrl: String?
 )
--- app/src/main/java/id/davidpratama/advweek4/viewmodel/ListViewModel.kt
+++ app/src/main/java/id/davidpratama/advweek4/viewmodel/ListViewModel.kt
@@ -1,23 +1,59 @@
 package id.davidpratama.advweek4.viewmodel
 
+import android.app.Application
+import android.util.Log
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import id.davidpratama.advweek4.model.Student
 
-class ListViewModel : ViewModel() {
+class ListViewModel (application: Application) :AndroidViewModel(application) {
 
     fun refresh(){
-        val student1 = Student("104710487","Zahara","2004/01/19","4855556814","http://dummyimage.com/100x75.png/cc0000/ffffff")
-        val student2 = Student("798523882","John","2012/08/13","3115111200","http://dummyimage.com/100x75.png/ff4444/ffffff")
-        val student3 = Student("418201142","Inga","1996/06/01","7511245662","http://dummyimage.com/100x75.png/cc0000/ffffff")
-
-        val students: ArrayList<Student> = arrayListOf(student1,student2,student3)
-
-        studentID.value = students
-        studentLoadError.value = false
+       studentLoadError.value = false
         studentLiveDone.value = true
+
+        queue = Volley.newRequestQueue( getApplication() )
+        val url = "http://adv.jitusolution.com/student.php"
+
+        val stringRequest = StringRequest(
+            Request.Method.GET, url,
+            {
+                response ->
+                val sType = object : TypeToken<List<Student>>() { }.type
+                val result = Gson().fromJson<List<Student>>(response, sType)
+                studentID.value = result
+                studentLiveDone.value = false
+
+                Log.d("showvoley", result.toString())
+
+                studentLiveDone.value = false
+                Log.d("showvoley", response.toString())
+            },
+            {
+                Log.d("showvoley", it.toString())
+                studentLoadError.value = false
+                studentLiveDone.value = false
+            })
+
+            stringRequest.tag = TAG
+             queue?.add(stringRequest)
+
     }
 
+    override fun onCleared() {
+        super.onCleared()
+        queue?.cancelAll(TAG)
+    }
+
+    val TAG = "volleyTag"
+    private var queue: RequestQueue? = null
     val studentID = MutableLiveData<List<Student>>()
     val studentLoadError = MutableLiveData<Boolean>()
     val studentLiveDone = MutableLiveData<Boolean>()

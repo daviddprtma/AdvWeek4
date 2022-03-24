e2014c0747e312249b8adac844e99ed9235420a4
daviddprtma <davidchristianpratama@gmail.com>
2022-03-24 09:46:57 +0700

    Update Week 7 RX Java & Notification

--- app/src/main/java/id/davidpratama/advweek4/view/MainActivity.kt
+++ app/src/main/java/id/davidpratama/advweek4/view/MainActivity.kt
@@ -2,11 +2,56 @@ package id.davidpratama.advweek4.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import id.davidpratama.advweek4.R
+import id.davidpratama.advweek4.util.createNotificationChannel
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.Observer
+import io.reactivex.rxjava3.disposables.Disposable
+import io.reactivex.rxjava3.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity() {
+    init {
+        instance = this
+    }
+    companion object{
+        private var instance : MainActivity ?= null
+
+        fun showNotification(title:String, content:String,icon:Int ){
+            val channelID = "${instance?.packageName} - {${instance?.getString(R.string.app_name)}"
+
+            val notificationBuilder = NotificationCompat.Builder(instance!!.applicationContext, channelID).apply {
+                setSmallIcon(icon)
+                setContentTitle(title)
+                setContentText(content)
+                setStyle(NotificationCompat.BigTextStyle())
+                priority = NotificationCompat.PRIORITY_DEFAULT
+                setAutoCancel(true)
+            }
+
+            val notificationManager = NotificationManagerCompat.from(instance!!.applicationContext.applicationContext!!)
+            notificationManager.notify(1001, notificationBuilder.build())
+
+
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        Observable.just("a stream of data","hellow","world")
+//            .subscribeOn(Schedulers.io())
+//            .observeOn(AndroidSchedulers.mainThread())
+//            .subscribe({ Log.d("Messages","data captured $it") },
+//                       {Log.d("Messages","error: ${it.message.toString()}")},
+//                       {Log.d("Messages","Messages complete")})
+
+        createNotificationChannel(this,
+            NotificationManagerCompat.IMPORTANCE_DEFAULT, false,
+            getString(R.string.app_name), "App notification channel.")
+
     }
 }
